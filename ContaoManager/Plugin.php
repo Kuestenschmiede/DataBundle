02620c90d1f4a28292ff9b746ac8798d29d43bf6
coastforge-cro <claudio.ross@kuestenschmiede.de>
2019-07-16 16:54:38 +0200

    Add basic file structure

---
+++ ContaoManager/Plugin.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace con4gis\MapContentBundle\ContaoManager;
+
+use con4gis\CoreBundle\con4gisCoreBundle;
+use con4gis\CloudBundle\con4gisCloudBundle;
+use con4gis\MapContentBundle\con4gisMapContentBundle;
+use con4gis\ProjectsBundle\con4gisProjectsBundle;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Config\ConfigInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Contao\ManagerPlugin\Config\ConfigPluginInterface;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * Gets a list of autoload configurations for this bundle.
+     *
+     * @param ParserInterface $parser
+     *
+     * @return ConfigInterface[]
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(con4gisMapContentBundle::class)
+                ->setLoadAfter([con4gisProjectsBundle::class]),
+        ];
+    }
+    
+}
---
+++ con4gisMapContentBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace con4gis\MapContentBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class con4gisMapContentBundle extends Bundle
+{
+    
+}
