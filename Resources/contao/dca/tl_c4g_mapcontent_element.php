6276954a8d9543b99daced80d01820c41d6a4266
rro <raphael.ross@kuestenschmiede.gmbh>
2019-10-24 15:28:41 +0200

    Change street number field from natural to text

--- Resources/contao/dca/tl_c4g_mapcontent_element.php
+++ Resources/contao/dca/tl_c4g_mapcontent_element.php
@@ -134,8 +134,8 @@ $addressName->eval()->class('w50');
 $addressStreet = new TextField('addressStreet', $dca);
 $addressStreet->eval()->class('clr w50');
 
-$addressNumber = new NaturalField('addressStreetNumber', $dca);
-$addressNumber->eval()->class('w50');
+$addressNumber = new TextField('addressStreetNumber', $dca);
+$addressNumber->sql("varchar(5) NOT NULL default ''")->eval()->class('w50');
 
 $addressZip = new TextField('addressZip', $dca);
 $addressZip->sql('char(5) NULL')
