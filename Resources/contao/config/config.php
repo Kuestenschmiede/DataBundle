e1a28bde522cea06c9355aad065befc324959aca
rro <raphael.ross@kuestenschmiede.gmbh>
2019-12-13 11:23:02 +0100

    Remove PublicEditableModule from backend select options

--- Resources/contao/config/config.php
+++ Resources/contao/config/config.php
@@ -62,9 +62,9 @@ $GLOBALS['TL_MODELS']['tl_c4g_mapcontent_element'] = MapcontentElementModel::cla
 $GLOBALS['TL_MODELS']['tl_c4g_mapcontent_custom_field'] = MapcontentCustomFieldModel::class;
 $GLOBALS['TL_MODELS']['tl_c4g_mapcontent_directory'] = MapcontentDirectoryModel::class;
 
-array_insert($GLOBALS['FE_MOD']['con4gis_mapcontent'], 1,
-    ['public_editable' => PublicEditableModule::class]
-);
+//array_insert($GLOBALS['FE_MOD']['con4gis_mapcontent'], 1,
+//    ['public_editable' => PublicEditableModule::class]
+//);
 
 array_insert($GLOBALS['FE_MOD']['con4gis_mapcontent'], 1,
     ['public_noneditable' => PublicNonEditableModule::class]
