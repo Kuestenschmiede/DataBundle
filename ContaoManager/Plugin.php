bd855baed0101536f28d6887c045b3726c229ffb
rro <raphael.ross@kuestenschmiede.gmbh>
2020-01-24 12:00:25 +0100

    Fix bundle configuration

--- ContaoManager/Plugin.php
+++ ContaoManager/Plugin.php
@@ -1,20 +1,25 @@
 <?php
 
+/**
+ * con4gis for Contao Open Source CMS
+ *
+ * @version   php 7
+ * @package   con4gis-Data (DataBundle)
+ * @author    con4gis contributors
+ * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright Küstenschmiede GmbH Software & Design 2011 - 2019
+ * @link      https://www.kuestenschmiede.de
+ */
+
 namespace con4gis\DataBundle\ContaoManager;
 
-use con4gis\CoreBundle\con4gisCoreBundle;
-use con4gis\CloudBundle\con4gisCloudBundle;
-use con4gis\DataBundle\con4GisDataContentBundle;
+use con4gis\DataBundle\con4gisDataBundle;
 use con4gis\MapsBundle\con4gisMapsBundle;
 use con4gis\ProjectsBundle\con4gisProjectsBundle;
-use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Config\ConfigInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
-use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
-use Symfony\Component\Config\Loader\LoaderResolverInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
 use Contao\ManagerPlugin\Config\ConfigPluginInterface;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
@@ -30,14 +35,14 @@ class Plugin implements BundlePluginInterface, ConfigPluginInterface
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create(con4GisDataContentBundle::class)
-                ->setLoadAfter([con4gisProjectsBundle::class, con4gisMapsBundle::class]),
+            BundleConfig::create(con4gisDataBundle::class)
+                ->setLoadAfter([con4gisProjectsBundle::class, con4gisMapsBundle::class])
         ];
     }
-    
+
     public function registerContainerConfiguration(LoaderInterface $loader, array $managerConfig)
     {
         $loader->load('@con4gisDataBundle/Resources/config/config.yml');
     }
-    
+
 }
---
+++ con4gisDataBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * con4gis for Contao Open Source CMS
+ *
+ * @version   php 7
+ * @package   con4gis-Data (DataBundle)
+ * @author    con4gis contributors
+ * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright Küstenschmiede GmbH Software & Design 2011 - 2019
+ * @link      https://www.kuestenschmiede.de
+ */
+
+namespace con4gis\DataBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+class con4gisDataBundle extends Bundle
+{
+
+}
