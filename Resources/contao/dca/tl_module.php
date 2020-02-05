1546fdeb12c3acce5d58853f76b5392bab954ac8
rro <raphael.ross@kuestenschmiede.gmbh>
2020-02-05 10:39:56 +0100

    Add missing exclude options

--- Resources/contao/dca/tl_module.php
+++ Resources/contao/dca/tl_module.php
@@ -16,6 +16,7 @@ $GLOBALS['TL_DCA']['tl_module']['palettes']['public_editable'] = '{title_legend}
 $GLOBALS['TL_DCA']['tl_module']['fields']['c4g_data_type'] =
     [
         'label'                   => &$GLOBALS['TL_LANG']['tl_module']['c4g_data_type'],
+        'exclude'                 => true,
         'inputType'               => 'select',
         'foreignKey'              => 'tl_c4g_data_type.name',
         'eval'                    => ['includeBlankOption' => true, 'multiple' => true, 'chosen' => true, 'class' => 'clr'],
@@ -25,6 +26,7 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['c4g_data_type'] =
 $GLOBALS['TL_DCA']['tl_module']['fields']['c4g_data_directory'] =
     [
         'label'                   => &$GLOBALS['TL_LANG']['tl_module']['c4g_data_directory'],
+        'exclude'                 => true,
         'inputType'               => 'select',
         'foreignKey'              => 'tl_c4g_data_directory.name',
         'eval'                    => ['includeBlankOption' => true, 'multiple' => true, 'chosen' => true, 'class' => 'clr'],
@@ -34,6 +36,7 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['c4g_data_directory'] =
 $GLOBALS['TL_DCA']['tl_module']['fields']['mapPage'] =
     [
         'label'                   => &$GLOBALS['TL_LANG']['tl_module']['mapPage'],
+        'exclude'                 => true,
         'inputType'               => 'pageTree',
         'sql'                     => "int(10) unsigned NOT NULL default '0'"
     ];
@@ -41,6 +44,7 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['mapPage'] =
 $GLOBALS['TL_DCA']['tl_module']['fields']['caption'] =
     [
         'label'                   => &$GLOBALS['TL_LANG']['tl_module']['caption'],
+        'exclude'                 => true,
         'inputType'               => 'text',
         'sql'                     => "varchar(255) NOT NULL default ''"
     ];
@@ -48,6 +52,7 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['caption'] =
 $GLOBALS['TL_DCA']['tl_module']['fields']['captionPlural'] =
     [
         'label'                   => &$GLOBALS['TL_LANG']['tl_module']['captionPlural'],
+        'exclude'                 => true,
         'inputType'               => 'text',
         'sql'                     => "varchar(255) NOT NULL default ''"
     ];
@@ -55,6 +60,7 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['captionPlural'] =
 $GLOBALS['TL_DCA']['tl_module']['fields']['showSelectFilter'] =
     [
         'label'                   => &$GLOBALS['TL_LANG']['tl_module']['showSelectFilter'],
+        'exclude'                 => true,
         'default'                 => false,
         'inputType'               => 'checkbox',
         'sql'                     => "char(1) NOT NULL default '0'"
@@ -63,6 +69,7 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['showSelectFilter'] =
 $GLOBALS['TL_DCA']['tl_module']['fields']['showFilterResetButton'] =
     [
         'label'                   => &$GLOBALS['TL_LANG']['tl_module']['showFilterResetButton'],
+        'exclude'                 => true,
         'default'                 => false,
         'inputType'               => 'checkbox',
         'sql'                     => "char(1) NOT NULL default '0'"
@@ -71,6 +78,7 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['showFilterResetButton'] =
 $GLOBALS['TL_DCA']['tl_module']['fields']['filterResetButtonCaption'] =
     [
         'label'                   => &$GLOBALS['TL_LANG']['tl_module'][''],
+        'exclude'                 => true,
         'default'                 => false,
         'inputType'               => 'text',
         'sql'                     => "varchar(255) NOT NULL default ''"
