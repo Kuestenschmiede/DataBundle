00ba493953082e460f10c1e2b26d2e5f0bea43ca
rro <raphael.ross@kuestenschmiede.gmbh>
2020-03-17 12:06:05 +0100

    Add mobile and email fields to tl_member_group

--- Resources/contao/dca/tl_member_group.php
+++ Resources/contao/dca/tl_member_group.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_member_group']['palettes']['default'] .= ';{data_bundle_legend},numberElements';
+$GLOBALS['TL_DCA']['tl_member_group']['palettes']['default'] .= ';{data_bundle_legend},numberElements,mobile,email';
 
 $GLOBALS['TL_DCA']['tl_member_group']['fields']['numberElements'] =
     [
@@ -10,4 +10,24 @@ $GLOBALS['TL_DCA']['tl_member_group']['fields']['numberElements'] =
         'inputType'               => 'text',
         'eval'                    => ['tl_class'=>'w50'],
         'sql'                     => "int(10) NOT NULL default '0'"
+    ];
+
+$GLOBALS['TL_DCA']['tl_member_group']['fields']['mobile'] =
+    [
+        'label'                   => &$GLOBALS['TL_LANG']['tl_member_group']['mobile'],
+        'exclude'                 => true,
+        'search'                  => true,
+        'inputType'               => 'text',
+        'eval'                    => ['maxlength'=>64, 'rgxp'=>'phone', 'decodeEntities'=>true, 'tl_class'=>'w50'],
+        'sql'                     => "varchar(64) NOT NULL default ''"
+    ];
+
+$GLOBALS['TL_DCA']['tl_member_group']['fields']['email'] =
+    [
+        'label'                   => &$GLOBALS['TL_LANG']['tl_member_group']['email'],
+        'exclude'                 => true,
+        'search'                  => true,
+        'inputType'               => 'text',
+        'eval'                    => ['maxlength'=>255, 'rgxp'=>'email', 'decodeEntities'=>true, 'tl_class'=>'w50'],
+        'sql'                     => "varchar(255) NOT NULL default ''"
     ];
--- Resources/contao/languages/de/tl_member_group.php
+++ Resources/contao/languages/de/tl_member_group.php
@@ -1,4 +1,6 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_member_group']['numberElements'] = ['Maximale Anzahl Elemente', 'Die maximale Anzahl Elemente, die diese Gruppe veröffentlichen darf.'];
+$GLOBALS['TL_LANG']['tl_member_group']['mobile'] = ['Mobiltelefonnummer', 'Mobiltelefonnummer der Gruppe.'];
+$GLOBALS['TL_LANG']['tl_member_group']['email'] = ['E-Mail-Adresse', 'E-Mail-Adresse der Gruppe.'];
 $GLOBALS['TL_LANG']['tl_member_group']['data_bundle_legend'] = 'con4gis DataBundle';
