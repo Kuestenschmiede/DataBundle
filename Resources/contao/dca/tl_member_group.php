33f149d620b25d9cb49a020686f61c60c5240fc6
rro <raphael.ross@kuestenschmiede.gmbh>
2020-03-18 10:29:47 +0100

    Add phone and address fields to tl_member_group

--- Resources/contao/dca/tl_member_group.php
+++ Resources/contao/dca/tl_member_group.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_member_group']['palettes']['default'] .= ';{data_bundle_legend},numberElements,mobile,email';
+$GLOBALS['TL_DCA']['tl_member_group']['palettes']['default'] .= ';{data_bundle_legend},numberElements,addressName,addressStreet,addressStreetNumber,addressZip,addressCity,addressState,addressCountry,phone,mobile,email';
 
 $GLOBALS['TL_DCA']['tl_member_group']['fields']['numberElements'] =
     [
@@ -10,6 +10,79 @@ $GLOBALS['TL_DCA']['tl_member_group']['fields']['numberElements'] =
         'inputType'               => 'text',
         'eval'                    => ['tl_class'=>'w50'],
         'sql'                     => "int(10) NOT NULL default '0'"
+    ];
+
+$GLOBALS['TL_DCA']['tl_member_group']['fields']['addressName'] =
+    [
+        'label'                   => &$GLOBALS['TL_LANG']['tl_member_group']['addressName'],
+        'exclude'                 => true,
+        'inputType'               => 'text',
+        'eval'                    => ['maxlength'=>255, 'tl_class'=>'w50 clr'],
+        'sql'                     => "varchar(255) NOT NULL default ''"
+    ];
+
+$GLOBALS['TL_DCA']['tl_member_group']['fields']['addressStreet'] =
+    [
+        'label'                   => &$GLOBALS['TL_LANG']['tl_member_group']['addressStreet'],
+        'exclude'                 => true,
+        'inputType'               => 'text',
+        'eval'                    => ['maxlength'=>255, 'tl_class'=>'w50'],
+        'sql'                     => "varchar(255) NOT NULL default ''"
+    ];
+
+$GLOBALS['TL_DCA']['tl_member_group']['fields']['addressStreetNumber'] =
+    [
+        'label'                   => &$GLOBALS['TL_LANG']['tl_member_group']['addressStreetNumber'],
+        'exclude'                 => true,
+        'inputType'               => 'text',
+        'eval'                    => ['maxlength'=>25, 'tl_class'=>'w50'],
+        'sql'                     => "varchar(25) NOT NULL default ''"
+    ];
+
+$GLOBALS['TL_DCA']['tl_member_group']['fields']['addressZip'] =
+    [
+        'label'                   => &$GLOBALS['TL_LANG']['tl_member_group']['addressZip'],
+        'exclude'                 => true,
+        'inputType'               => 'text',
+        'eval'                    => ['maxlength'=>10, 'tl_class'=>'w50'],
+        'sql'                     => "varchar(10) NOT NULL default ''"
+    ];
+
+$GLOBALS['TL_DCA']['tl_member_group']['fields']['addressCity'] =
+    [
+        'label'                   => &$GLOBALS['TL_LANG']['tl_member_group']['addressCity'],
+        'exclude'                 => true,
+        'inputType'               => 'text',
+        'eval'                    => ['maxlength'=>255, 'tl_class'=>'w50'],
+        'sql'                     => "varchar(255) NOT NULL default ''"
+    ];
+
+$GLOBALS['TL_DCA']['tl_member_group']['fields']['addressState'] =
+    [
+        'label'                   => &$GLOBALS['TL_LANG']['tl_member_group']['addressState'],
+        'exclude'                 => true,
+        'inputType'               => 'text',
+        'eval'                    => ['maxlength'=>255, 'tl_class'=>'w50'],
+        'sql'                     => "varchar(255) NOT NULL default ''"
+    ];
+
+$GLOBALS['TL_DCA']['tl_member_group']['fields']['addressCountry'] =
+    [
+        'label'                   => &$GLOBALS['TL_LANG']['tl_member_group']['addressCountry'],
+        'exclude'                 => true,
+        'inputType'               => 'text',
+        'eval'                    => ['maxlength'=>255, 'tl_class'=>'w50'],
+        'sql'                     => "varchar(255) NOT NULL default ''"
+    ];
+
+$GLOBALS['TL_DCA']['tl_member_group']['fields']['phone'] =
+    [
+        'label'                   => &$GLOBALS['TL_LANG']['tl_member_group']['phone'],
+        'exclude'                 => true,
+        'search'                  => true,
+        'inputType'               => 'text',
+        'eval'                    => ['maxlength'=>64, 'rgxp'=>'phone', 'decodeEntities'=>true, 'tl_class'=>'w50 clr'],
+        'sql'                     => "varchar(64) NOT NULL default ''"
     ];
 
 $GLOBALS['TL_DCA']['tl_member_group']['fields']['mobile'] =
--- Resources/contao/languages/de/tl_member_group.php
+++ Resources/contao/languages/de/tl_member_group.php
@@ -1,6 +1,14 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_member_group']['numberElements'] = ['Maximale Anzahl Elemente', 'Die maximale Anzahl Elemente, die diese Gruppe veröffentlichen darf.'];
+$GLOBALS['TL_LANG']['tl_member_group']['addressName'] = ['Name', 'Namensbestandteil der Adresse (Firma, Inhaber, etc.)'];
+$GLOBALS['TL_LANG']['tl_member_group']['addressStreet'] = ['Straße', 'Straße ohne Hausnummer.'];
+$GLOBALS['TL_LANG']['tl_member_group']['addressStreetNumber'] = ['Hausnummer', ''];
+$GLOBALS['TL_LANG']['tl_member_group']['addressZip'] = ['Postleitzahl', ''];
+$GLOBALS['TL_LANG']['tl_member_group']['addressCity'] = ['Ort', ''];
+$GLOBALS['TL_LANG']['tl_member_group']['addressState'] = ['Bundesland', ''];
+$GLOBALS['TL_LANG']['tl_member_group']['addressCountry'] = ['Staat', ''];
+$GLOBALS['TL_LANG']['tl_member_group']['phone'] = ['Telefonnummer', 'Telefonnummer der Gruppe.'];
 $GLOBALS['TL_LANG']['tl_member_group']['mobile'] = ['Mobiltelefonnummer', 'Mobiltelefonnummer der Gruppe.'];
 $GLOBALS['TL_LANG']['tl_member_group']['email'] = ['E-Mail-Adresse', 'E-Mail-Adresse der Gruppe.'];
 $GLOBALS['TL_LANG']['tl_member_group']['data_bundle_legend'] = 'con4gis DataBundle';
