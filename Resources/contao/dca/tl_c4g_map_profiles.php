8a624842501572f9249da19cfe767da97c5dd4ae
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2021-04-28 17:37:29 +0200

    Fixed typo

--- Resources/contao/dca/tl_c4g_map_profiles.php
+++ Resources/contao/dca/tl_c4g_map_profiles.php
@@ -1,5 +1,5 @@
 <?php
-$GLOBALS['TL_DCA']['tl_c4g_map_profiles']['palettes']['default'] = str_replace("{locstyle_legend:hide},label_color,resize_locstyles_zoom;", "{locstyle_legend:hide},label_color,resize_locstyles_zoom;{filter_legend},filterTypeData;", $GLOBALS['TL_DCA']['tl_c4g_map_profiles']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_c4g_map_profiles']['palettes']['default'] = str_replace("filters,filterHandling,filterResetButton", "filters,filterHandling,filterResetButton,filterTypeData", $GLOBALS['TL_DCA']['tl_c4g_map_profiles']['palettes']['default']);
 $GLOBALS['TL_DCA']['tl_c4g_map_profiles']['fields']['filterTypeData'] = [
     'label'                   => &$GLOBALS['TL_LANG']['tl_c4g_map_profiles']['filterTypeData'],
     'exclude'                 => true,
--- Resources/contao/languages/de/tl_c4g_map_profiles.php
+++ Resources/contao/languages/de/tl_c4g_map_profiles.php
@@ -1,4 +1,3 @@
 <?php
-$GLOBALS['TL_LANG']['tl_c4g_map_profiles']['filter_legend'] = "Filter";
 
-$GLOBALS['TL_LANG']['tl_c4g_map_profiles']['filterTypeData'] = ['Filter parralel darstellen', 'Wählen Sie, ob die Filter nebeneinander, statt in einem Dropdown dargestellt werden sollen.'];
+$GLOBALS['TL_LANG']['tl_c4g_map_profiles']['filterTypeData'] = ['Filter parallel darstellen', 'Wählen Sie, ob die Filter nebeneinander, statt in einem Dropdown dargestellt werden sollen.'];
