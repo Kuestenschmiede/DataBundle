4519f2d2e24492ea79e769657d971c9c19073fb1
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2021-05-06 23:27:55 +0200

    Interim status for con4gis 8

--- Resources/contao/languages/en/tl_c4g_map_profiles.php
+++ Resources/contao/languages/en/tl_c4g_map_profiles.php
@@ -1,3 +1,12 @@
 <?php
+/*
+ * This file is part of con4gis, the gis-kit for Contao CMS.
+ * @package con4gis
+ * @version 8
+ * @author con4gis contributors (see "authors.txt")
+ * @license LGPL-3.0-or-later
+ * @copyright (c) 2010-2021, by Küstenschmiede GmbH Software & Design
+ * @link https://www.con4gis.org
+ */
 
 $GLOBALS['TL_LANG']['tl_c4g_map_profiles']['filterTypeData'] = ['Display filters paralel', 'Choose wether you want to display the filter next to each other instead of in a dropdown.'];
--- Resources/contao/languages/en/tl_member_group.php
+++ Resources/contao/languages/en/tl_member_group.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file is part of con4gis, the gis-kit for Contao CMS.
+ * @package con4gis
+ * @version 8
+ * @author con4gis contributors (see "authors.txt")
+ * @license LGPL-3.0-or-later
+ * @copyright (c) 2010-2021, by Küstenschmiede GmbH Software & Design
+ * @link https://www.con4gis.org
+ */
 
 $GLOBALS['TL_LANG']['tl_member_group']['numberElements'] = ['Maximum number of elements', 'The maximum number of elements this group is allowed to publish.'];
 $GLOBALS['TL_LANG']['tl_member_group']['addressName'] = ['name', 'Name component of the address (company, owner, etc.)'];
